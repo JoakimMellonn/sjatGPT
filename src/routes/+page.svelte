a7878e19957e4ae22c4596f6a183b7b03cc53777
Joakim Rosenfeldt Pedersen <joakim@mellonn.com>
2025-03-05 17:30:43 +0100

    changed to sjat

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,6 +22,7 @@
 
 	let drink: Drink | undefined = $state();
 	let loading: boolean = $state(false);
+	let serialConnected: boolean = $state(false);
 
 	onMount(async () => {
 		const alcoholicsResult = localStorage.getItem('alcoholics');
@@ -33,12 +34,22 @@
 		if (othersResult) {
 			others = JSON.parse(othersResult);
 		}
+
+		const ports = await navigator.serial.getPorts();
+		if (ports.length === 1) {
+			startSerial(ports[0]);
+		}
 	});
 
 	async function requestPort() {
 		const port = await navigator.serial.requestPort();
 		console.log(port);
 
+		startSerial(port);
+	}
+
+	async function startSerial(port: any) {
+		serialConnected = true;
 		await port.open({ baudRate: 9600 });
 		console.log(port.readable);
 
@@ -60,7 +71,7 @@
 					}
 				}
 			} catch (error) {
-				// TODO: Handle non-fatal read error.
+				serialConnected = false;
 			}
 		}
 	}
@@ -150,25 +161,34 @@
 			`And here is the list of other types of drinks: ${JSON.stringify(others)}.`
 		);
 		prompt = prompt.concat('Respond using JSON.');
-		console.log(prompt);
 		return prompt;
 	}
 </script>
 
-<div class="grid min-h-screen w-screen grid-cols-2 grid-rows-6 bg-purple-900">
+<div class="grid min-h-screen w-screen grid-cols-2 grid-rows-6 bg-purple-400 dark:bg-purple-900">
 	<div class="col-span-2 row-span-2 place-content-center justify-items-center">
 		<Card.Root class="min-w-xl  w-1/2">
 			<Card.Header>
-				<Card.Title>SlatGPT</Card.Title>
-				<Card.Description>Angiv lager forneden, tryk på knappen og lad magien ske</Card.Description>
+				<Card.Title>SjatGPT</Card.Title>
+				{#if serialConnected}
+					<Card.Description
+						>Angiv lager forneden, tryk på knappen og lad magien ske</Card.Description
+					>
+				{:else}
+					<Card.Description
+						>Tilslut en arduino og vælg den ved at trykke på knappen herunder.</Card.Description
+					>
+				{/if}
 			</Card.Header>
 			<Card.Content>
-				<Button
-					onclick={() => {
-						requestPort();
-					}}>Request port</Button
-				>
-				{#if drink}
+				{#if !serialConnected}
+					<Button
+						class="w-full"
+						onclick={() => {
+							requestPort();
+						}}>Request port</Button
+					>
+				{:else if drink}
 					<Card.Title>Alkohol</Card.Title>
 					<ul class="list-disc">
 						{#each drink.alcoholics as alcohol}
@@ -185,79 +205,83 @@
 					<p>Tryk på knappen herunder og lad magien ske...</p>
 				{/if}
 			</Card.Content>
-			<Card.Footer>
-				<Button
-					class="w-full"
-					disabled={loading}
-					onclick={() => {
-						requestDrink();
-					}}>Bestil drink</Button
-				>
-			</Card.Footer>
+			{#if serialConnected}
+				<Card.Footer>
+					<Button
+						class="w-full"
+						disabled={loading}
+						onclick={() => {
+							requestDrink();
+						}}>Bestil drink</Button
+					>
+				</Card.Footer>
+			{/if}
 		</Card.Root>
 	</div>
-	<div class="row-span-4 justify-items-center p-16">
-		<Card.Root class="w-full">
-			<Card.Header>
-				<Card.Title>Alkohol</Card.Title>
-				<Card.Description>Angiv hvilke slags alkohol, der er på lager</Card.Description>
-			</Card.Header>
-			<Card.Content>
-				<Button
-					class="w-full"
-					onclick={() => {
-						alcoholDialogOpen = true;
-					}}>Tilføj Alkohol</Button
-				>
-				<Separator class="my-4" />
-				{#if alcoholics.length > 0}
-					{#each alcoholics as alcohol}
-						<Card.Root class="mt-6 flex w-full justify-between p-6 first:mt-0">
-							<Card.Title class="mt-2">{alcohol}</Card.Title>
+	{#if serialConnected}
+		<div class="row-span-4 justify-items-center p-16">
+			<Card.Root class="w-full">
+				<Card.Header>
+					<Card.Title>Alkohol</Card.Title>
+					<Card.Description>Angiv hvilke slags alkohol, der er på lager</Card.Description>
+				</Card.Header>
+				<Card.Content>
+					<Button
+						class="w-full"
+						onclick={() => {
+							alcoholDialogOpen = true;
+						}}>Tilføj Alkohol</Button
+					>
+					<Separator class="my-4" />
+					{#if alcoholics.length > 0}
+						{#each alcoholics as alcohol}
+							<Card.Root class="mt-6 flex w-full justify-between p-6 first:mt-0">
+								<Card.Title class="mt-2">{alcohol}</Card.Title>
 
-							<Button onclick={(_) => removeAlcohol(alcohol)} variant="outline" size="icon">
-								<Trash2 />
-								<span class="sr-only">Remove {alcohol}</span>
-							</Button>
-						</Card.Root>
-					{/each}
-				{:else}
-					<p>Tilføj noget alkohol for at begynde...</p>
-				{/if}
-			</Card.Content>
-		</Card.Root>
-	</div>
-	<div class="row-span-4 justify-items-center p-16">
-		<Card.Root class="w-full">
-			<Card.Header>
-				<Card.Title>Opblanding</Card.Title>
-				<Card.Description>Angiv hvilke slags opblandinger, der er på lager</Card.Description>
-			</Card.Header>
-			<Card.Content>
-				<Button
-					class="w-full"
-					onclick={() => {
-						otherDialogOpen = true;
-					}}>Tilføj Opblanding</Button
-				>
-				<Separator class="my-4" />
-				{#if others.length > 0}
-					{#each others as other}
-						<Card.Root class="mt-6 flex w-full justify-between p-6 first:mt-0">
-							<Card.Title class="mt-2">{other}</Card.Title>
+								<Button onclick={(_) => removeAlcohol(alcohol)} variant="outline" size="icon">
+									<Trash2 />
+									<span class="sr-only">Remove {alcohol}</span>
+								</Button>
+							</Card.Root>
+						{/each}
+					{:else}
+						<p>Tilføj noget alkohol for at begynde...</p>
+					{/if}
+				</Card.Content>
+			</Card.Root>
+		</div>
+		<div class="row-span-4 justify-items-center p-16">
+			<Card.Root class="w-full">
+				<Card.Header>
+					<Card.Title>Opblanding</Card.Title>
+					<Card.Description>Angiv hvilke slags opblandinger, der er på lager</Card.Description>
+				</Card.Header>
+				<Card.Content>
+					<Button
+						class="w-full"
+						onclick={() => {
+							otherDialogOpen = true;
+						}}>Tilføj Opblanding</Button
+					>
+					<Separator class="my-4" />
+					{#if others.length > 0}
+						{#each others as other}
+							<Card.Root class="mt-6 flex w-full justify-between p-6 first:mt-0">
+								<Card.Title class="mt-2">{other}</Card.Title>
 
-							<Button onclick={(_) => removeOther(other)} variant="outline" size="icon">
-								<Trash2 />
-								<span class="sr-only">Remove {other}</span>
-							</Button>
-						</Card.Root>
-					{/each}
-				{:else}
-					<p>Tilføj en opblanding for at begynde...</p>
-				{/if}
-			</Card.Content>
-		</Card.Root>
-	</div>
+								<Button onclick={(_) => removeOther(other)} variant="outline" size="icon">
+									<Trash2 />
+									<span class="sr-only">Remove {other}</span>
+								</Button>
+							</Card.Root>
+						{/each}
+					{:else}
+						<p>Tilføj en opblanding for at begynde...</p>
+					{/if}
+				</Card.Content>
+			</Card.Root>
+		</div>
+	{/if}
 </div>
 
 <Dialog.Root bind:open={alcoholDialogOpen}>
